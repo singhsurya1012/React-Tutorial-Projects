736701f19bdf7cb0749ec40136006f9e00909107
Surya <singhsurya1012@gmail.com>
2020-09-14 00:39:18 +0530

    Form Validation Added

--- burger-builder/src/components/UI/Button/Button.js
+++ burger-builder/src/components/UI/Button/Button.js
@@ -7,7 +7,8 @@ const Button = (props) => {
     return (
         <button
             className={[classes.Button, classes[props.btnType]].join(' ')}
-            onClick={props.clicked}>{props.children}</button>
+            onClick={props.clicked}
+            disabled={props.disabled}> {props.children} </button>
     )
 }
 
--- burger-builder/src/containers/Checkout/Checkout.js
+++ burger-builder/src/containers/Checkout/Checkout.js
@@ -10,19 +10,19 @@ class Checkout extends Component {
         price: 0
     }
 
-    componentWillMount () {
-        const query = new URLSearchParams( this.props.location.search );
+    componentWillMount() {
+        const query = new URLSearchParams(this.props.location.search);
         const ingredients = {};
         let price = 0;
-        for ( let param of query.entries() ) {
+        for (let param of query.entries()) {
             // ['salad', '1']
             if (param[0] === 'price') {
-                price = param[1];
+                price = +param[1];
             } else {
                 ingredients[param[0]] = +param[1];
             }
         }
-        this.setState( { ingredients: ingredients, totalPrice: price } );
+        this.setState({ ingredients: ingredients, totalPrice: price.toFixed(2) });
     }
 
     checkoutCancelledHandler = () => {
@@ -30,18 +30,18 @@ class Checkout extends Component {
     }
 
     checkoutContinuedHandler = () => {
-        this.props.history.replace( '/checkout/contact-data' );
+        this.props.history.replace('/checkout/contact-data');
     }
 
-    render () {
+    render() {
         return (
             <div>
                 <CheckoutSummary
                     ingredients={this.state.ingredients}
                     checkoutCancelled={this.checkoutCancelledHandler}
                     checkoutContinued={this.checkoutContinuedHandler} />
-                <Route 
-                    path={this.props.match.path + '/contact-data'} 
+                <Route
+                    path={this.props.match.path + '/contact-data'}
                     render={(props) => (<ContactData ingredients={this.state.ingredients} price={this.state.totalPrice} {...props} />)} />
             </div>
         );
